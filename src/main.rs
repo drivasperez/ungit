c1ac1872dda3fc22840ac3ecd33230428fbd2773
Daniel Rivas Perez <Daniel.Rivas@huddle.com>
2020-08-20 09:29:44 +0100

    Switch to surf 2 alpha (why not), get hash

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use async_std::task;
 use flate2::read::GzDecoder;
+use http_types::StatusCode;
+use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
 use tar::Archive;
 use thiserror::Error;
@@ -7,9 +9,13 @@ use thiserror::Error;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "gitter", about = "Just making a CLI")]
 struct Opt {
+    /// The user
+    #[structopt()]
+    user: String,
     /// The repository to load
     #[structopt()]
     repository: String,
+    /// The location the repository should be unpacked to
     #[structopt()]
     target: Option<String>,
 }
@@ -19,9 +25,9 @@ enum GitterError {
     #[error("Could not find package")]
     NotFound,
     #[error("Network error")]
-    NetworkError(Box<dyn std::error::Error + Send + Sync + 'static>),
+    NetworkError(http_types::Error),
     #[error("Unknown status code")]
-    UnknownStatusCode(u16),
+    UnknownStatusCode(StatusCode),
     #[error("Too many redirects")]
     TooManyRedirects,
     #[error("IO Error")]
@@ -34,10 +40,12 @@ fn main() -> Result<()> {
     let options = Opt::from_args();
     task::block_on(async {
         let uri = format!(
-            "https://github.com/{}/archive/master.tar.gz",
-            options.repository
+            "https://codeload.github.com/{}/{}/tar.gz/master",
+            options.user, options.repository
         );
         println!("Fetching latest from {}", uri);
+        let sha = fetch_latest_sha(&options.user, &options.repository).await?;
+        println!("Latest commit hash: {}", sha);
         fetch_bytes(&uri).await
     })
     .and_then(|bytes| {
@@ -62,19 +70,48 @@ async fn fetch_bytes(uri: &str) -> Result<Vec<u8>> {
 
     println!("Status code: {}", res.status());
 
-    match res.status().as_u16() {
-        200 => res.body_bytes().await.map_err(|e| GitterError::IOError(e)),
-        404 => Err(GitterError::NotFound),
-        302 => {
-            res = surf::get(res.header("location").unwrap())
+    match res.status() {
+        StatusCode::Ok => res.body_bytes().await.map_err(|e| GitterError::IOError(e)),
+        StatusCode::NotFound => Err(GitterError::NotFound),
+        StatusCode::Found => {
+            let loc = res.header("location").unwrap().as_str();
+            println!("Redirecting to {}", loc);
+            res = surf::get(loc)
                 .await
                 .map_err(|e| GitterError::NetworkError(e))?;
-            if res.status().as_u16() != 200 {
+            if res.status() != StatusCode::Ok {
                 Err(GitterError::TooManyRedirects)
             } else {
                 res.body_bytes().await.map_err(|e| GitterError::IOError(e))
             }
         }
-        n => Err(GitterError::UnknownStatusCode(n)),
+        status => Err(GitterError::UnknownStatusCode(status)),
     }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Commit {
+    sha: String,
+    node_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Branch {
+    name: String,
+    commit: Commit,
+}
+
+async fn fetch_latest_sha(owner: &str, repo: &str) -> Result<String> {
+    let uri = format!(
+        "https://api.github.com/repos/{}/{}/branches/master",
+        owner, repo
+    );
+
+    let mut res = surf::get(uri)
+        .await
+        .map_err(|e| GitterError::NetworkError(e))?;
+
+    let repo: Branch = res.body_json().await.map_err(|e| GitterError::IOError(e))?;
+
+    Ok(repo.commit.sha.to_owned())
 }
